3f47411b267489aa99060885fae4038adc5ed6eb
Ye Hua <yehua@Yes-MacBook-Pro.local>
2017-03-23 15:03:35 -0400

    two versions

---
+++ v2/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AlertModule } from 'ng2-bootstrap';
+import { DropdownModule } from 'ng2-bootstrap/dropdown';
+import { AgmCoreModule } from 'angular2-google-maps/core';
+import { IndexComponent } from './index/index.component';
+import { TweetThumbnailComponent } from './tweets/tweet-thumbnail.component';
+
+@NgModule({
+  declarations: [
+    IndexComponent,
+    TweetThumbnailComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AlertModule.forRoot(),
+    DropdownModule.forRoot(),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCvSxaeNAxaziOKTelih7xX3OxeCa6_2Zk'
+    })  
+  ],
+  providers: [],
+  bootstrap: [IndexComponent]
+})
+export class AppModule { }
